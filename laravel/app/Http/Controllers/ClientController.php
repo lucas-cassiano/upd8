01c0d96f4161ab597331e66010f42c265ac0556e
lucas-cassiano <45541119+lucas-cassiano@users.noreply.github.com>
2023-06-11 21:46:32 -0300

    Finalizado o cadastro de cliente

--- laravel/app/Http/Controllers/ClientController.php
+++ laravel/app/Http/Controllers/ClientController.php
@@ -3,16 +3,149 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Client;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
 
 class ClientController extends Controller
 {
 
-    public function cadastro(Request $request){
+    public function cadastro(Request $request): JsonResponse{
+
+        try{
+
+            $validator = Validator::make($request->all(), [
+                'nome' => 'required|string|max:255',
+                'cpf' => 'required|string|unique:client|max:11',
+                'nascimento' => 'required|max:10',
+                'sexo' => 'required|string|max:1',
+                'endereco' => 'required|string|max:255',
+                'estado' => 'required|string|max:2',
+                'cidade' => 'required|string|max:255'
+            ]);
+
+            if($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }else if($this->validarCPF($request->input('cpf'))==false){
+                return response()->json(['cpf' => [
+                    'invalido'
+                ]], 400);
+            }else if($this->validarData($request->input('nascimento'))==false){
+                return response()->json(['nasicmento' => [
+                    'invalido'
+                ]], 400);
+            }else if($request->input('sexo') !== 'M' && $request->input('sexo') !== 'F'){
+                return response()->json(['sexo' => [
+                    'invalido'
+                ]], 400);
+            }
+
+            $client = new Client();
+            $client->nome = $request->input('nome');
+            $client->cpf = $request->input('cpf');
+            $client->nascimento = $this->converteDate($request->input('nascimento'));
+            $client->sexo = $request->input('sexo');
+            $client->endereco = $request->input('endereco');
+            $client->estado = $request->input('estado');
+            $client->cidade = $request->input('cidade');
+            $client->save();
+
+            return response()->json([
+                'success' => true,
+                'status' => 'Deu certo'
+            ], 200);
+
+        }catch(Exception $e){
+            return response()->json([
+                'success' => false
+            ], 401);
+        }
+
+    }
+
+    private function converteDate(string $data){
+
+        if (!preg_match('/^(\d{2})\/(\d{2})\/(\d{4})$/', $data, $matches)) {
+            return '';
+        }
+
+        $dia = intval($matches[1]);
+        $mes = intval($matches[2]);
+        $ano = intval($matches[3]);
+
+        return $ano.'-'.$mes.'-'.$dia;
 
     }
 
     public function filtro(Request $request){
 
     }
+
+    private function validarData($data) {
+
+        if (!preg_match('/^(\d{2})\/(\d{2})\/(\d{4})$/', $data, $matches)) {
+            return false;
+        }
+
+        $dia = intval($matches[1]);
+        $mes = intval($matches[2]);
+        $ano = intval($matches[3]);
+
+        if ($dia < 1 || $dia > 31 || $mes < 1 || $mes > 12 || $ano < 1000 || $ano > 9999) {
+            return false;
+        }
+
+        if (($mes == 4 || $mes == 6 || $mes == 9 || $mes == 11) && $dia > 30) {
+            return false;
+        }
+
+        if ($mes == 2) {
+            if (($ano % 4 == 0 && $ano % 100 != 0) || $ano % 400 == 0) {
+                if ($dia > 29) {
+                    return false;
+                }
+            } else {
+                if ($dia > 28) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+
+    private function validarCPF($cpf) {
+
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        if (preg_match('/^(\d)\1+$/', $cpf)) {
+            return false;
+        }
+
+        for ($i = 9; $i < 11; $i++) {
+            $soma = 0;
+            for ($j = 0; $j < $i; $j++) {
+                $soma += $cpf[$j] * (($i + 1) - $j);
+            }
+            $resto = $soma % 11;
+            if ($resto < 2) {
+                $digito = 0;
+            } else {
+                $digito = 11 - $resto;
+            }
+            if ($cpf[$i] != $digito) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
 }
