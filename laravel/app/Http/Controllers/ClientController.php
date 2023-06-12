bd1f05673f97e3ae2396a8caf3bbd2df2c8f748d
lucas-cassiano <45541119+lucas-cassiano@users.noreply.github.com>
2023-06-11 22:24:52 -0300

    Finalizado método para o filtro do cliente

--- laravel/app/Http/Controllers/ClientController.php
+++ laravel/app/Http/Controllers/ClientController.php
@@ -77,7 +77,88 @@ class ClientController extends Controller
 
     }
 
-    public function filtro(Request $request){
+    public function filtro(Request $request): JsonResponse{
+
+        try{
+
+            $validator = Validator::make($request->all(), [
+                'nome' => 'string|max:255',
+                'cpf' => 'string|unique:client|max:11',
+                'nascimento' => 'max:10',
+                'sexo' => 'string|max:1',
+                'endereco' => 'string|max:255',
+                'estado' => 'string|max:2',
+                'cidade' => 'string|max:255'
+            ]);
+
+            if($validator->fails()) {
+             //   return response()->json($validator->errors(), 400);
+            }else if($request->input('cpf')  && $this->validarCPF($request->input('cpf'))==false){
+                return response()->json(['cpf' => [
+                    'invalido'
+                ]], 400);
+            }else if($request->input('nascimento') && $this->validarData($request->input('nascimento'))==false){
+                return response()->json(['nasicmento' => [
+                    'invalido'
+                ]], 400);
+            }else if($request->input('sexo') && $request->input('sexo') !== 'M' && $request->input('sexo') !== 'F'){
+                return response()->json(['sexo' => [
+                    'invalido'
+                ]], 400);
+            }
+
+            $query = Client::query();
+
+            if($request->input('nome')){
+                $nome = $request->input('nome');
+                $query->orWhere('nome', 'LIKE', "%{$nome}%");
+            }
+
+            if($request->input('cpf')){
+                $query->orWhere('cpf', '=', $request->input('cpf'));
+            }
+
+            if($request->input('nascimento')){
+                $query->orWhere('nascimento', '=', $this->converteDate($request->input('nascimento')));
+            }
+
+            if($request->input('sexo')){
+                $query->orWhere('sexo', '=', $request->input('sexo'));
+            }
+
+            if($request->input('estado')){
+                $query->orWhere('estado', '=', $request->input('estado'));
+            }
+
+            if($request->input('cidade')){
+                $query->orWhere('cidade', '=', $request->input('cidade'));
+            }
+
+            $clients = $query->get();
+
+            $getInfo = [];
+            foreach($clients as $client){
+                $getInfo[] = [
+                    'nome' => $client['nome'],
+                    'cpf' => $client['nome'],
+                    'nascimento' => $client['nascimento'],
+                    'sexo' => $client['sexo'],
+                    'estado' => $client['estado'],
+                    'cidade' => $client['cidade'],
+                ];
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => $getInfo
+            ], 200);
+
+        }catch(Exception $e){
+            return response()->json([
+                'success' => false,
+                'data' => []
+            ], 401);
+        }
 
     }
 
